7dd7b7f141b9eb4d7a280ebdaf99d41ef070cf17
Ivancahwagu <ivantheo159@gmail.com>
2024-09-30 17:25:13 +0700

    top

--- components/Tombolubahtema.js
+++ components/Tombolubahtema.js
@@ -2,6 +2,6 @@
 
 export default function Tombolubahtema() {
     return (
-        <div className="fixed w-[2000px] left-[25%] h-[500%] top-[-45%] overflow-hidden bg-gradient-to-r from-transparent via-lime-400/80 to-transparent z-[-2] animate-spin-slow"></div>
+        <div className="fixed w-[2000px] left-[25%] h-[500%] top-[-45%] overflow-hidden bg-gradient-to-r from-transparent dark:via-lime-400/80 via-blue-800/80 to-transparent z-[-2] animate-spin-slow"></div>
     )
 }
